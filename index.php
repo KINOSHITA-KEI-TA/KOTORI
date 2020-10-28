9ebad7e3c611cc9e6b91b0494d5c6c37ec4d7370
KINOSHITA-KEI-TA <kei.quality3@gmail.com>
2020-10-28 15:23:49 +0900

    CSS装飾

--- index.php
+++ index.php
@@ -17,29 +17,38 @@ function h ($s){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="index.css">
   <title>旅の一覧</title>
 </head>
 <body>
-<h2>旅の一覧</h2>
-<p><a href="/project/form.html">新規作成</a></p>
-  <table>
-    <tr>
-      <th>タイトル</th>
-      <th>カテゴリ</th>
-      <th>日記日時</th>
-    </tr>
-    <?php foreach($tripDATA as $column): ?>
-    <tr>
-      <td><?php echo h($column['title'] )?></td>
-      <td><?php echo h($trip->setCategoryName($column['category'])) ?></td>
-      <td><?php echo h($column['post_at']) ?></td>
-      <td><a href= "/project/detail.php?id=<?php echo $column['id'] ?>">詳細</a></td>
-      <td><a href= "/project/update_form.php?id=<?php echo $column['id'] ?>">編集</a></td>
-      <td><a href= "/project/trip_delete.php?id=<?php echo $column['id'] ?>">削除</a></td>
-    </tr>
-    <?php endforeach; ?>
+<div class="header">
+  <h2 class="title">＜私の旅日記＞</h2>
+</div>
+<p class="new-content"><a href="/project/form.html">→ 新しく投稿</a></p>
+  <h3>これまでの旅の記録</h3>
+  <div class="content">
+    <table>
+      <div class="title-content">
+        <tr>
+          <th>タイトル</th>
+          <th>カテゴリ</th>
+          <th>日記日時</th>
+        </tr>
+      </div>
+      <div class="enter-content">
+        <?php foreach($tripDATA as $column): ?>
+        <tr class="enter-block">
+          <td class="block"><?php echo h($column['title'] )?></td>
+          <td class="block"><?php echo h($trip->setCategoryName($column['category'])) ?></td>
+          <td class="block"><?php echo h($column['post_at']) ?></td>
+          <td class="block"><a href= "/project/detail.php?id=<?php echo $column['id'] ?>">詳細</a></td>
+          <td class="block"><a href= "/project/update_form.php?id=<?php echo $column['id'] ?>">編集</a></td>
+          <td class="block"><a href= "/project/trip_delete.php?id=<?php echo $column['id'] ?>">削除</a></td>
+        </tr>
+        <?php endforeach; ?>
+      </div>
 
-  </table>
-  
+    </table>
+  </div>  
 </body>
 </html>
--- update_form.php
+++ update_form.php
@@ -16,28 +16,33 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="form.css">
   <title>旅日記更新</title>
 </head>
 <body>
-  <h2>投稿フォーム</h2>
-  <form action="/project/trip_update.php" method="POST">
-  <input type="hidden" name= "id" value="<?php echo $id ?>">
-  <p>旅のタイトル：</p>
-  <input type="text" name="title" value="<?php echo $title ?>">
-  <p>日記の本文：</p>
-  <textarea name="content" id="content" cols="30" rows="10"><?php echo $content ?></textarea>
-  <br>
-  <p>カテゴリ：</p>
-  <select name="category">
-    <option value="1" <?php if($category === 1 ) echo "selected"?>>日常 </option>
-    <option value="2" <?php if($category === 2 ) echo "selected"?>>旅の記録</option>
-  </select>
-  <br>
-  <input type="radio" name="publish_status" value="1"<?php if($publish_status === 1 ) echo "checked"?>>公開
-  <input type="radio" name="publish_status" value="2"<?php if($publish_status === 2 ) echo "checked"?>>非公開
-  <br>
-  <input type="submit" value="投稿">
-</form>
+  <div class="Form">
+    <h2>投稿フォーム</h2>
+    <form action="/project/trip_update.php" method="POST">
+    <input type="hidden" name= "id"  value="<?php echo $id ?>">
+    <p class="form-font">旅のタイトル：</p>
+    <input type="text" name="title" id="form-title" value="<?php echo $title ?>">
+    <p class="form-font">日記の本文：</p>
+    <textarea name="content" id="content" cols="30" rows="10"><?php echo $content ?></textarea>
+    <br>
+    <div class="category-box">
+      <p class="form-font">カテゴリ：</p>
+      <select name="category" id="form-category">
+        <option value="1" <?php if($category === 1 ) echo "selected"?>>日常 </option>
+        <option value="2" <?php if($category === 2 ) echo "selected"?>>旅の記録</option>
+      </select>
+    </div>
+    <br>
+    <input type="radio" name="publish_status" value="1"<?php if($publish_status === 1 ) echo "checked"?>>公開
+    <input type="radio" name="publish_status" value="2"<?php if($publish_status === 2 ) echo "checked"?>>非公開
+    <br>
+    <input type="submit" value="投稿" class="form-submit">
+    </form>
+  </div>
 <p><a href="/">もどる</a></p>
 </body>
 </html>
